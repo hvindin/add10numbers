c18b807c9730e0cbab43cd7dc5b7dac688f07d56
Henry Vindin <hvindin@westpac.com.au>
2016-10-07 05:13:53 +0000

    init

---
+++ sum.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/zsh
+sum=0
+total=
+while [ ! $sum -eq 10 ]; do
+  let sum=$(( $sum + 1 ))
+  input=
+  while [ ! $input ]; do
+    echo -n "input number: "
+    read input
+  done
+  let total=$(( $total + $input ))
+  echo $total
+done
